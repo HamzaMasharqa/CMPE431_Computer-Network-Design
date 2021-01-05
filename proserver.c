673e63a7fbd3ac02404f66d055ec2801de20bc17
HamzaMasharqa <ham.ham258951@gmail.com>
2021-01-05 22:14:50 +0200

    Vresion 0.0.6 with switch case to help the client choose the command but still has a bug and security.ini

--- proserver.c
+++ proserver.c
@@ -1,171 +1,169 @@
 #include <sys/types.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <sys/socket.h>
+
 #include <netinet/in.h>
+
 #include <arpa/inet.h>
+
 #include <stdio.h>
+
 #include <sys/types.h>
+
 #include <dirent.h>
+
 #include <errno.h>
+
 #include <string.h>
+
 #include <stdlib.h>
+
+//#define ls 5863588
 
 #define DEFAULT_BUFLEN 1024
 #define PORT 2428
 
+void readDir(int fd) {
 
+  int length, rcnt;
+  char recvbuf[DEFAULT_BUFLEN], bmsg[DEFAULT_BUFLEN];
+  int recvbuflen = DEFAULT_BUFLEN;
 
+  DIR * pDIR;
 
+  struct dirent * pDirEnt;
+
+  pDIR = opendir("HI");
+
+  if (pDIR == NULL) {
+    fprintf(stderr, "%s %d: opendir() failed (%s)\n",
+      __FILE__, __LINE__, strerror(errno));
+    exit(-1);
+  }
+
+  rcnt = send(fd, "Those are the files in your directory", strlen("Those are the files in your directory"), 0);
+
+  pDirEnt = readdir(pDIR);
+  while (pDirEnt != NULL) {
+
+    rcnt = send(fd, pDirEnt -> d_name, strlen(pDirEnt -> d_name), 0);
+    rcnt = send(fd, "\n", strlen("\n"), 0);
+
+    //   printf( "%s\n", pDirEnt->d_name ,'\n','\n','\n');
+    pDirEnt = readdir(pDIR);
+
+  }
+
+  closedir(pDIR);
+
+}
 
 void do_job(int fd) {
-int length,rcnt;
-char recvbuf[DEFAULT_BUFLEN],bmsg[DEFAULT_BUFLEN];
-int  recvbuflen = DEFAULT_BUFLEN;
-int userN,passW;
-char command[50];
+  int length, rcnt;
+  char recvbuf[DEFAULT_BUFLEN], bmsg[DEFAULT_BUFLEN];
+  int recvbuflen = DEFAULT_BUFLEN;
 
-    char Welcomsge[100] = "Welcome to the Hamza's server  \n";
+  char choise[DEFAULT_BUFLEN];
+
+  char Welcomsge[100] = "Welcome to the Hamza's server  \n";
   //   char userName[100] = "Please Enter your username : \n";
-     
-   //   char passWord[100] = "Please enter your password :\n";
-    
-   rcnt = send(fd, Welcomsge, strlen(Welcomsge), 0);
-    //rcnt = send(fd, userName, strlen(userName), 0);
+
+  //   char passWord[100] = "Please enter your password :\n";
+
+  rcnt = send(fd, Welcomsge, strlen(Welcomsge), 0);
+  //rcnt = send(fd, userName, strlen(userName), 0);
   //  rcnt = send(fd, passWord, strlen(passWord), 0);
+
+  do {
+    rcnt = send(fd, "please choose one of the commands : \n ", strlen("please choose one of the commands : \n "), 0);
+    rcnt = recv(fd, recvbuf , recvbuflen, 0);
     
     
-    do {
-     rcnt = recv(fd, recvbuf, recvbuflen, 0);
-     
-     printf(recvbuf);
-     
-     if (recvbuf == "ls"){
-     ///////////////////////////////////////////////////////////////////////////////////////////
-  
- DIR *pDIR;
- 
-    struct dirent *pDirEnt;
+    printf(recvbuf);
+    
+    strcpy(*choise,recvbuf);
+    printf(choise);
+    switch (choise[0]) {
 
+    case 'l':
 
-    pDIR = opendir("HI");
+      readDir(fd);
 
-    if ( pDIR == NULL ) {
-        fprintf( stderr, "%s %d: opendir() failed (%s)\n",
-                __FILE__, __LINE__, strerror( errno ));
-        exit( -1 );
+      break;
+
+    default:
+
+      printf("wrong choise");
+
     }
+  } while (rcnt > 0);
+  //////////////////////////////////////////////////////////////////////////////////////////
 
-   rcnt = send(fd, "Those are the files in your directory" , strlen("Those are the files in your directory" ), 0);
-
-    pDirEnt = readdir( pDIR );
-    while ( pDirEnt != NULL ) {
-    
-    
-    rcnt = send(fd, pDirEnt->d_name , strlen(pDirEnt->d_name ), 0);
-    rcnt = send(fd, "\n" , strlen("\n" ), 0);
-    
-     //   printf( "%s\n", pDirEnt->d_name ,'\n','\n','\n');
-        pDirEnt = readdir( pDIR );
-       
-    }
-
-
-    closedir( pDIR );
-}}while (rcnt > 0);
- //////////////////////////////////////////////////////////////////////////////////////////
- 
-    do {
-        rcnt = recv(fd, recvbuf, recvbuflen, 0);
-        if (rcnt > 0) {
-           
-
-       
-        rcnt = send( fd, recvbuf, rcnt, 0 );
-            if (rcnt < 0) {
-                printf("Send failed:\n");
-                close(fd);
-                break;
-            }
-        
-
-        }
-        else if (rcnt == 0)
-            printf("Connection closing...\n");
-        else  {
-            printf("Receive failed:\n");
-            close(fd);
-            break;
-        }
-    } while (rcnt > 0);
 }
 
+int main() {
+  int server, client;
+  struct sockaddr_in local_addr;
+  struct sockaddr_in remote_addr;
+  int length, fd, rcnt, optval;
+  pid_t pid;
 
-
-int main()
-{
-int server, client;
-struct sockaddr_in local_addr;
-struct sockaddr_in remote_addr;
-int length,fd,rcnt,optval;
-pid_t pid;
-
-
-if ((server = socket( AF_INET, SOCK_STREAM, 0)) < 0 ) { 
+  if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("Can't create socket!");
-    return(1);
-}
+    return (1);
+  }
 
-memset( &local_addr, 0, sizeof(local_addr) );
-memset( &remote_addr, 0, sizeof(remote_addr) );
+  memset( & local_addr, 0, sizeof(local_addr));
+  memset( & remote_addr, 0, sizeof(remote_addr));
 
-local_addr.sin_family = AF_INET;
-local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-local_addr.sin_port = htons(PORT);
+  local_addr.sin_family = AF_INET;
+  local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  local_addr.sin_port = htons(PORT);
 
+  optval = 1;
+  setsockopt(server, SOL_SOCKET, SO_REUSEADDR, & optval, sizeof optval);
 
-optval = 1;
-setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+  if (bind(server, (struct sockaddr * ) & local_addr, sizeof(local_addr)) < 0) {
 
-if ( bind( server, (struct sockaddr *)&local_addr, sizeof(local_addr) ) < 0 )
-{
-   
     perror("Bind error");
-    return(1);
-}
+    return (1);
+  }
 
-if ( listen( server, SOMAXCONN ) < 0 ) {
-        perror("listen");
-        exit(1);
-}
+  if (listen(server, SOMAXCONN) < 0) {
+    perror("listen");
+    exit(1);
+  }
 
-printf("Concurrent  socket server now starting on port %d\n",PORT);
-printf("Wait for connection\n");
+  printf("Concurrent  socket server now starting on port %d\n", PORT);
+  printf("Wait for connection\n");
 
-while(1) {  
+  while (1) {
     length = sizeof remote_addr;
-    if ((fd = accept(server, (struct sockaddr *)&remote_addr, \
-          &length)) == -1) {
-          perror("Accept Problem!");
-          continue;
+    if ((fd = accept(server, (struct sockaddr * ) & remote_addr,  &
+        length)) == -1) {
+      perror("Accept Problem!");
+      continue;
     }
     printf("Server: got connection from %s\n", \
-            inet_ntoa(remote_addr.sin_addr));
+      inet_ntoa(remote_addr.sin_addr));
 
-   
-    if ((pid=fork()) == 0) {
-        close(server);
-        do_job(fd);
-        
-        printf("Child finished their job!\n");
-        close(fd);
-        exit(0);
+    if ((pid = fork()) == 0) {
+      close(server);
+      do_job(fd);
+
+      printf("Child finished their job!\n");
+      close(fd);
+      exit(0);
     }
 
-}
+  }
 
-
-close(server);
+  close(server);
 
 }
