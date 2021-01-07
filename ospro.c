d5100921eb9fce04ca5977bc9f699dcf033e6c5d
HamzaMasharqa <ham.ham258951@gmail.com>
2021-01-07 21:40:58 +0200

    First Commit

---
+++ ospro.c
@@ -0,0 +1,74 @@
+#include <sys/types.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+
+
+
+
+int main (){
+	int choise;
+	
+	
+	
+	
+	printf("%s", "\t \t \t \t CPU Scheduler Simulator\n");
+	printf("%s","1) Scheduling Method \n");
+	printf("%s","2) Preemptive Mode \n");
+	printf("%s","3) Non-preemptive Mode \n");
+	printf("%s","4) Show Result \n");
+	printf("%s","5) End Program \n");
+	printf("%s","Option> ");
+	scanf("%d",&choise);
+	
+	
+	switch(choise){
+		
+		case 1:
+			
+			break;
+			
+			case 2:
+				
+				break;
+				
+				case 3:
+					
+					break;
+					
+					case 4:
+						
+						break;
+						
+						case 5:
+							exit(0);
+							
+							break;
+							
+							default:
+								printf("%s","Please choose one of the comands in the list :)");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
+
